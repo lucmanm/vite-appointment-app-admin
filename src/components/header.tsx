15caeb642c207eb1b7971b68d247a5fa57621122
lucmanm <greenjamez22@gmail.com>
2024-12-24 01:28:46 +0300

    update user nav ui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,12 +9,13 @@ import {
 } from "./ui/breadcrumb";
 import { Separator } from "./ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
+import { UserNav } from "./user-nav";
 
 export default function Header() {
   const pathname = useLocation().pathname.split("/").pop()?.replace(/-/, " ");
 
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12  justify-between">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
@@ -37,6 +38,7 @@ export default function Header() {
           </BreadcrumbList>
         </Breadcrumb>
       </div>
+      <UserNav/>
     </header>
   );
 }
